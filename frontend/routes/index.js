b78a3f21bcc972deb2da338dd39ce63a34b5526d
Falape <filipe_s_fernandes@hotmail.com>
2023-06-18 16:20:12 +0100

    added signup page

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -4,11 +4,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('header', { title: 'Express' });
+  res.redirect('/login');
 });
 
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Express' });
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Express' });
 });
 
 router.get('/recurso', function(req, res, next) {
