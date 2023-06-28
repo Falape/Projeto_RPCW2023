20c2674ee83897dec996dfed52bcab5b62740dc5
Falape <filipe_s_fernandes@hotmail.com>
2023-06-28 12:55:29 +0100

    fix delete user bug when admin delete user

--- frontend/routes/admin.js
+++ frontend/routes/admin.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
-const {renderListRoleUpdateRequests} = require('../public/javascripts/renderPages')
+const {renderListRoleUpdateRequests, renderNoticiasPage} = require('../public/javascripts/renderPages')
 
 router.get('/updateRequests', function(req, res, next) {
     if (!req.session.user) {
@@ -56,5 +56,61 @@ router.get('/requestRoleUpdate/refuse/:id', function(req, res, next) {
     });
 });
 
+
+router.get('/delete/:id', function(req, res, next) {
+    console.log("delete by id with admin")
+    console.log(req.session)
+    if (!req.session.user) {
+      return res.redirect('/login');
+    }
+  
+    //console.log(req.body);
+  
+    console.log(req.params.id)
+    axios.delete(process.env.API_AUTH_URL + '/admin/deleteUser/' + req.params.id, {
+                headers: {
+                  Authorization: `Bearer ${req.session.user.token}`
+                }
+              })
+              .then((resp) => {
+                //console.log(response);
+                axios.delete(process.env.API_DATA_URL + '/api/delete/hard/user/' + req.params.id, {
+                  headers: {
+                    Authorization: `Bearer ${req.session.user.token}`
+                  }
+                })
+                .then((resp1) => {
+                  console.log("Apagou recursos do utilizador e comentários que estes tinha")
+                  axios.delete(process.env.API_DATA_URL + '/api/delete/hard/comments/'  + req.params.id, {
+                    headers: {
+                      Authorization: `Bearer ${req.session.user.token}`
+                    }
+                  })
+                  .then((resp2) => {
+                    console.log("Apagou os comentários")
+                  })
+                  .catch((error) => {
+                    console.log(error);
+                    //res.render('error_page', { message: error.response.data.error });
+                  });
+                })
+                .catch((error) => {
+                  console.log(error);
+                  //res.render('error_page', { message: error.response.data.error });
+                });
+                //res.render('user_page', { user: response.data, owner:true, admin:false, requestRoleUpdateFlag:true });
+                renderNoticiasPage(res,req,{},true, null, null, null);
+                //res.render('noticias', { userInfo: req.session.user, userDeletedFlag: true});
+              })
+              .catch((error) => {
+                //console.log(error);
+                //res.render('error_page', { message: error.response.data.error });
+                if (error.response && error.response.data){
+                  renderUserPage(req, res, true, null, null, null, false,error.response.data.error);
+                }else{
+                  res.render('error_page', { message: error });
+                }
+            });
+  });
 
 module.exports = router;
