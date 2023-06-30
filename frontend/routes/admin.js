c4f530cd732f444ea63bbe93049abc859cf85cb8
Falape <filipe_s_fernandes@hotmail.com>
2023-06-30 17:16:11 +0100

    change endpoints of accept/refuse userRequestUpdateRole

--- frontend/routes/admin.js
+++ frontend/routes/admin.js
@@ -87,19 +87,31 @@ router.post('/updatePassword/:id', function(req, res, next) {
       }
 });
 
-router.get('/requestRoleUpdate/accept/:id', function (req, res, next) {
+router.get('/requestRoleUpdate/:id', function (req, res, next) {
   if (!req.session.user) {
     return res.redirect('/login');
   }
-  axios.get(process.env.API_AUTH_URL + '/admin/updateRole/accept/' + req.params.id, {
+  console.log(req.query.accept)
+  if(req.query.accept!=undefined){
+
+  axios.get(process.env.API_AUTH_URL + '/admin/updateRoleAcceptRefuse/' + req.params.id, {
+    params: {
+      accept: req.query.accept
+    },
     headers: {
       Authorization: `Bearer ${req.session.user.token}`
     }
   })
     .then((response) => {
       console.log(response.data)
-      req.session.alerts = {
-        acceptedFlag: true,
+      if(req.query.accept == "true"){
+        req.session.alerts = {
+          acceptedFlag: true,
+        }
+      }else{
+        req.session.alerts = {
+          acceptedFlag: false,
+        }
       }
       res.redirect('/admin/updateRequests')
       //renderListRoleUpdateRequests(req, res, true, null)
@@ -117,39 +129,41 @@ router.get('/requestRoleUpdate/accept/:id', function (req, res, next) {
         res.render('error_page', { message: error });
       }
     });
+  }else
+    res.render('error_page', { message: "Missing accept parameter!" });
 });
 
-router.get('/requestRoleUpdate/refuse/:id', function (req, res, next) {
-  if (!req.session.user) {
-    return res.redirect('/login');
-  }
-  axios.get(process.env.API_AUTH_URL + '/admin/updateRole/refuse/' + req.params.id, {
-    headers: {
-      Authorization: `Bearer ${req.session.user.token}`
-    }
-  })
-    .then((response) => {
-      console.log(response.data)
-      req.session.alerts = {
-        acceptedFlag: true,
-      }
-      res.redirect('/admin/updateRequests')
-      //renderListRoleUpdateRequests(req, res, false, null)
-    })
-    .catch((error) => {
-      console.log(error);
-      if (error.response && error.response.data) {
-        req.session.alerts = {
-          errorFlag: true,
-          msg: error.response.data.error
-        }
-        res.redirect('/admin/updateRequests')
-        //renderListRoleUpdateRequests(req, res, null, error.response.data.error)
-      } else {
-        res.render('error_page', { message: error });
-      }
-    });
-});
+// router.get('/requestRoleUpdate/refuse/:id', function (req, res, next) {
+//   if (!req.session.user) {
+//     return res.redirect('/login');
+//   }
+//   axios.get(process.env.API_AUTH_URL + '/admin/updateRole/refuse/' + req.params.id, {
+//     headers: {
+//       Authorization: `Bearer ${req.session.user.token}`
+//     }
+//   })
+//     .then((response) => {
+//       console.log(response.data)
+//       req.session.alerts = {
+//         acceptedFlag: true,
+//       }
+//       res.redirect('/admin/updateRequests')
+//       //renderListRoleUpdateRequests(req, res, false, null)
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//       if (error.response && error.response.data) {
+//         req.session.alerts = {
+//           errorFlag: true,
+//           msg: error.response.data.error
+//         }
+//         res.redirect('/admin/updateRequests')
+//         //renderListRoleUpdateRequests(req, res, null, error.response.data.error)
+//       } else {
+//         res.render('error_page', { message: error });
+//       }
+//     });
+// });
 
 
 router.get('/delete/:id', function (req, res, next) {
