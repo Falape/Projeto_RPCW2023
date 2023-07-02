f453ca78087da69e9a2f47d32afe4772cdd84eb0
AROM98 <rafa.olim@sapo.pt>
2023-07-02 09:29:17 +0100

    deleting resources #45  - fixed

--- data_api/routes/resourceRoute.js
+++ data_api/routes/resourceRoute.js
@@ -181,7 +181,7 @@ router.put('/edit/:id', checkValidTokenProducer, function (req, res) {
 
 
 // delete resource (hard)
-router.delete('/delete/hard/:id', checkValidTokenAdmin, function (req, res) {
+router.delete('/delete/hard/:id', checkValidTokenProducer, function (req, res) {
     re_id = req.params.id
     resourceController.deleteResourceMEGA(re_id)
         .then(resources => {
